73846c64a020ba77f45469179340756b2c84f683
Trentini1 <erick.trentini@icloud.com>
2025-10-07 16:04:33 -0300

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,19 @@
+// js/firebase.js
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAHq-utRHf-rDVJ9YtNdD8PCIRvboGqLuI",
+    authDomain: "gestor-pro-51706.firebaseapp.com",
+    projectId: "gestor-pro-51706",
+    storageBucket: "gestor-pro-51706.appspot.com",
+    messagingSenderId: "519113320151",
+    appId: "1:519113320151:web:d22638842180572b9338fd"
+};
+
+const app = initializeApp(firebaseConfig);
+// Exporta as instâncias para que outros arquivos possam usá-las
+export const auth = getAuth(app);
+export const db = getFirestore(app);
